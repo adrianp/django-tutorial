1c00180be7c01322f3d61495d9354a37565df84e
Adrian-Tudor Panescu <adrian@panescu.com>
2016-01-20 17:01:49 +0200

    basic question views

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,22 @@ from django.conf.urls import url
 from . import views
 
 
+app_name = 'polls'
+
 urlpatterns = [
+    # polls
     url(r'^$', views.index, name='index'),
-    url(r'ping/$', views.ping, name='ping')
+
+    # polls/ping
+    url(r'ping/$', views.ping, name='ping'),
+
+    # polls/5
+    # (?P<foo>) is a capturing group named foo
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # polls/5/result
+    url(r'^(?P<question_id>[0-9]+)/result$', views.result, name='result'),
+
+    # polls/5/vote
+    url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,27 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 
 def index(req):
-    return HttpResponse('Hello world, you are at the polls index.')
+    question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(req, 'polls/index.html', {'question_list': question_list})
 
 
 def ping(req):
     return HttpResponse('PONG')
+
+
+def detail(req, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(req, 'polls/detail.html', {'question': question})
+
+
+def result(req, question_id):
+    return HttpResponse('You are looking at the results of question %s' %
+                        question_id)
+
+
+def vote(req, question_id):
+    return HttpResponse('You are voting on question %s.' % question_id)
