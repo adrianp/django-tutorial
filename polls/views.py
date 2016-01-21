7ba7b377412bbdc98fee84b9a7bb8e60007a356c
Adrian-Tudor Panescu <adrian@panescu.com>
2016-01-21 12:09:01 +0200

    polls/views.py: don't display future questions

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import reverse
 from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -19,7 +20,9 @@ def ping(req):
 class IndexView(generic.ListView):
     def get_queryset(self):
         """Return the last five published questions"""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
