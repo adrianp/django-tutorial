0ca9e2721e3fcf65a442d3f0a388f69c111fe1e0
Adrian-Tudor Panescu <adrian@panescu.com>
2016-01-21 10:55:24 +0200

    test.py: future question test case

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,17 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+
+class QuestionMethodTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        Question.was_published_recently() should return False for future
+        questions
+        """
+        time = timezone.now() + datetime.timedelta(days=365)
+        future_question = Question(pub_date=time)
+        self.assertFalse(future_question.was_published_recently())
