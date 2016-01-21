2c60599240c2b6562fdf36ceb8a2aa8b955033e0
Adrian-Tudor Panescu <adrian@panescu.com>
2016-01-21 11:46:17 +0200

    polls/test.py: tests was_published_recently

--- polls/tests.py
+++ polls/tests.py
@@ -9,9 +9,25 @@ from .models import Question
 class QuestionMethodTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         """
-        Question.was_published_recently() should return False for future
+        Question.was_published_recently should return False for future
         questions
         """
         time = timezone.now() + datetime.timedelta(days=365)
         future_question = Question(pub_date=time)
         self.assertFalse(future_question.was_published_recently())
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        Question.was_published_recently should return False for old questions
+        """
+        time = timezone.now() - datetime.timedelta(days=1)
+        past_question = Question(pub_date=time)
+        self.assertFalse(past_question.was_published_recently())
+
+    def test_was_published_recently_with_new_question(self):
+        """
+        Question.was_published_recently should return True for new questions
+        """
+        time = timezone.now() - datetime.timedelta(hours=23)
+        new_question = Question(pub_date=time)
+        self.assertTrue(new_question.was_published_recently())
